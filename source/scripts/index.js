62c45c4954dfe8513ca970d507e6ccfc10458e86
danskor <skorokhodd@yandex.ru>
2024-01-06 23:42:16 +0300

    feat: add mob styles to index

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,8 @@
 /* в этот файл добавляет скрипты*/
+const nav = document.querySelector('.main-nav');
+const navButton = document.querySelector('.burger');
+
+navButton.addEventListener('click', () => {
+  navButton.classList.toggle('burger--active');
+  nav.classList.toggle('main-nav--active');
+})
