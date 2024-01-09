21686bf5b82aec2d611db68b578fd104580738b4
danskor <skorokhodd@yandex.ru>
2024-01-09 17:45:17 +0300

    feat: little edits before review

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -5,4 +5,4 @@ const navButton = document.querySelector('.burger');
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('burger--active');
   nav.classList.toggle('main-nav--active');
-})
+});
