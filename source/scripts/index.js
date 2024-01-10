9bb3a79aa34fc5ab8cb5eecb8ce52725e61070b0
danskor <skorokhodd@yandex.ru>
2024-01-10 23:21:08 +0300

    feat: add media quaries to catalog and form (desctop)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -2,6 +2,8 @@
 const nav = document.querySelector('.main-nav');
 const navButton = document.querySelector('.burger');
 
+nav.classList.remove('main-nav--nojs');
+
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('burger--active');
   nav.classList.toggle('main-nav--active');
