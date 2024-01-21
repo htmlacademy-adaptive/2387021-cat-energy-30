2c86f22d6e5d549c98c136f10f7558108fd223aa
DanSkor <skorokhodd@yandex.ru>
2024-01-21 19:59:29 +0300

    fix: edits after review

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,8 +3,13 @@ const nav = document.querySelector('.main-nav');
 const navButton = document.querySelector('.burger');
 
 nav.classList.remove('main-nav--nojs');
+navButton.classList.remove('burger--nojs');
 
 navButton.addEventListener('click', () => {
   navButton.classList.toggle('burger--active');
   nav.classList.toggle('main-nav--active');
 });
+
+const map = document.querySelector('.map__image-reserve');
+
+map.classList.remove('map__image-reserve--nojs');
